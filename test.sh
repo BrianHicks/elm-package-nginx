3046c29cbca50e5f458bb380b6984af096dd4cc6
Brian Hicks <brian@brianthicks.com>
2018-04-30 11:07:19 -0600

    add basic config and test script

---
+++ test.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+set -xeuo pipefail
+
+EIGHTEEN=$(docker run --rm --detach --name eighteen brianhicks/dummy:0.18)
+NINETEEN=$(docker run --rm --detach --name nineteen brianhicks/dummy:0.18)
+
+ROUTER=$(docker run --rm --detach --link eighteen --link nineteen --publish 8080:80 --volume "$PWD/nginx.conf":/etc/nginx/nginx.conf:ro nginx nginx-debug -g 'daemon off;')
+
+finish() {
+    docker logs "$ROUTER"
+    docker stop "$EIGHTEEN" "$NINETEEN" "$ROUTER"
+}
+trap finish EXIT
+
+curl localhost:8080
